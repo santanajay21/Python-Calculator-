2ba875de80a5492929bf9b477a9065aaf3f1eb24
Jeremy Santana <72940290+santanajay21@users.noreply.github.com>
2025-02-15 23:57:15 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from art import logo
 
-
+"""Below is the calculations of eachh operation in the calculator"""
 def add(n1, n2):
     return n1 + n2
 
@@ -11,8 +11,11 @@ def multiply(n1,n2):
     return n1 * n2
 
 def  divide(n1,n2):
+    if n2 == 0:
+        return "Error: Division by zero"
     return n1 / n2
 
+"""Below is the dictionary of the operations"""
 operations = {
     "+": add,
     "-": subtract,
@@ -20,3 +23,29 @@ operations = {
     "/": divide,
 }
 
+"""Below is the calculator function"""
+def calculator():
+    num1 =  float(input("What's the first number?: "))
+
+    while True:
+        opp = input("Pick an operation: +, -, *, /: ")
+        if opp not in operations:
+            print("Invalid operation")
+            continue
+
+        num2 = float(input("What's the next number?: "))
+        function = operations[opp]
+        result = function(num1, num2)
+
+        print(f"{num1} {opp} {num2} = {result}")
+
+        any_more_operations = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+        if any_more_operations == 'y':
+            num1 = result
+        elif any_more_operations == 'n':
+                calculator()
+        else:
+                print("Invalid input")
+                break
+
+calculator()
